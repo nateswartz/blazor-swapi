7a99372a3934dc21035470b31c6b8d740eb4cfff
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-05-04 13:01:36 -0400

    Some refactoring and updates
    Move data classes to new files
    Change page from displaying species to displaying characters and their associated species

---
+++ NateSwartz.Blazor.SwApi/Models/People.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace NateSwartz.Blazor.SwApi.Models
+{
+    public class PeopleResponse
+    {
+        public int count { get; set; }
+        public List<Person> results { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public string[] species { get; set; }
+        public string species_name { get; set; }
+    }
+}
---
+++ NateSwartz.Blazor.SwApi/Models/Species.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace NateSwartz.Blazor.SwApi.Models
+{
+    public class SpeciesResponse
+    {
+        public int count { get; set; }
+        public List<Species> results { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Species
+    {
+        public string name { get; set; }
+        public string classification { get; set; }
+        public string designation { get; set; }
+        public string average_height { get; set; }
+        public string skin_colors { get; set; }
+        public string language { get; set; }
+
+    }
+}
